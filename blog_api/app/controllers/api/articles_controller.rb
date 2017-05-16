1f9abdef9429bbb64b60a9ab2f7ccd7092bcdf81
at-trangtruong1 <tnkttruong@gmail.com>
2017-05-16 15:48:53 +0700

    update

--- blog_api/app/controllers/api/articles_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  class ArticlesController < ApplicationController
-
-    def index
-      @articles = Article.all
-      render json: @articles, each_serializer: Article::IndexSerializer
-    end
-  end
-end
